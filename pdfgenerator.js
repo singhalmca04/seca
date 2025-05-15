5b7949dd573f0883baa897232e9f1a11f4428180
Vinay Singhal <v.singhal@huulke.com>
2025-05-15 11:45:51 +0530

    cc

--- pdfgenerator.js
+++ pdfgenerator.js
@@ -1,4 +1,5 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 const Handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
@@ -12,13 +13,20 @@ const compileTemplate = (templatePath, data) => {
 
 const generatePDF = async (templatePath, data) => {
     const html = compileTemplate(templatePath, data);
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
+
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
     const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
     await browser.close();
     return pdfBuffer;
 };
+
 
 module.exports = {
     generatePDF
