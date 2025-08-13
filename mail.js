e5f691e96160cd918e3437a7090ff46fe059b9be
Vinay Singhal <v.singhal@huulke.com>
2025-08-13 14:23:28 +0530

    mail

--- mail.js
+++ mail.js
@@ -1,52 +1,52 @@
 const nodemailer = require('nodemailer');
 
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: 'srmhallticket@gmail.com',
-//         pass: 'bgjw bfan wglo dgjj' // Not your real password!
-//     }
-// });
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'srmhallticket@gmail.com',
+        pass: 'bgjw bfan wglo dgjj' // Not your real password!
+    }
+});
 
-// exports.sendMail = (mailOptions) => {
-//     transporter.sendMail(mailOptions, function (error, info) {
-//         if (error) {
-//             console.log(error);
-//         } else {
-//             console.log('Email sent: ' + info.response);
-//         }
-//     });
-// }
+exports.sendMail = (mailOptions) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
 
 // import nodemailer from "nodemailer";
 
-export async function sendMail(mailOptions) {
-  const { to, subject, text, html } = mailOptions;
+// export async function sendMail(mailOptions) {
+//   const { to, subject, text, html } = mailOptions;
 
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SENDINBLUE_USER,
-        pass: process.env.SENDINBLUE_PASS
-      }
-    });
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp-relay.brevo.com",
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.SENDINBLUE_USER,
+//         pass: process.env.SENDINBLUE_PASS
+//       }
+//     });
 
-    const info = await transporter.sendMail({
-      from: `"My App" <${process.env.SENDINBLUE_USER}>`,
-      to,
-      subject,
-      text,
-      html
-    });
-    console.log("Email sent: " + info.messageId);
-    return info;
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+//     const info = await transporter.sendMail({
+//       from: `"My App" <${process.env.SENDINBLUE_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html
+//     });
+//     console.log("Email sent: " + info.messageId);
+//     return info;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 
 
 // import { Resend } from 'resend';
