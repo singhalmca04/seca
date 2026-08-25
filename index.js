8c703b11ad301d0ce248640f6e349e7b8bb7da49
Vinay Singhal <vinayk@srmist.edu.in>
2026-08-25 13:55:30 +0530

    revert changes

--- index.js
+++ index.js
@@ -350,11 +350,7 @@ app.post('/uploadexcel', uploadx.single('file'), (req, res) => {
                 {
                     $set: {
                         subcode: subcode,
-                        semester: x.semester?.trim().toUpperCase(),
-                        section: x.section?.trim().toUpperCase(),
-                        branch: x.branch?.trim().toUpperCase(),
-                        specialization: x.specialization?.trim().toUpperCase(),
-                        batch: x.batch?.trim()
+                        semester: x.semester?.trim().toUpperCase()
                     },
                     $setOnInsert: {
                         name: x.Name?.trim().toUpperCase(),
